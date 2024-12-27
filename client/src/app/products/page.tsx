57b9020d5c4f1ddb0370c55c24a66bed180c9e87
arunikax <srarfian@gmail.com>
2024-12-27 16:03:42 +0700

    Product Page Done

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
-import { useGetProductsQuery } from "@/state/api";
-import { SearchIcon } from "lucide-react";
+import { useCreateProductMutation, useGetProductsQuery } from "@/state/api";
+import { PlusCircleIcon, SearchIcon } from "lucide-react";
 import { useState } from "react";
+import Header from "@/app/(components)/header";
+import Rating from "@/app/(components)/Rating";
+import CreateProductModal from "./CreateProductModal";
+
+type ProductFormData = {
+  name: string;
+  price: number;
+  stockQuantity: number;
+  rating: number;
+};
 
 const Products = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -13,6 +23,12 @@ const Products = () => {
     isLoading,
     isError,
   } = useGetProductsQuery(searchTerm);
+
+  const [createProduct] = useCreateProductMutation();
+
+  const handleCreateProduct = async (productData: ProductFormData) => {
+    await createProduct(productData);
+  };
 
   if (isLoading) {
     return <div className="py-4">Loading...</div>;
@@ -40,6 +56,55 @@ const Products = () => {
           />
         </div>
       </div>
+
+      {/* Header bar */}
+      <div className="flex justify-between items-center mb-6">
+        <Header name="Products" />
+        <button
+          className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+          onClick={() => setIsModalOpen(true)}
+        >
+          <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create
+          Product
+        </button>
+      </div>
+
+      {/* BODY PRODUCT LIST */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-10 justify-between">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          products?.map((p) => (
+            <div
+              key={p.productId}
+              className="border shadow rounded-md p-4 max-w-full w-full mx-auto "
+            >
+              <div className="flex flex-col items-center">
+                img
+                <h3 className="text-lg text-gray-900 font-semibold">
+                  {p.name}
+                </h3>
+                <p className="text-gray-800">${p.price.toFixed(2)}</p>
+                <div className="text-sm text-gray-600 mt-1">
+                  Stock: {p.stockQuantity}
+                </div>
+                {p.rating && (
+                  <div className="flex items-center mt-2">
+                    <Rating rating={p.rating} />
+                  </div>
+                )}
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+
+      {/* MODAL */}
+      <CreateProductModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onCreate={handleCreateProduct}
+      />
     </div>
   );
 };
